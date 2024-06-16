ac9dba245922e9a701d179f77bcf03d11ed47f10
Christopher Geary <hello@crgeary.com>
2024-06-16 20:13:05 +0100

    feat: Add button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["components/**/*.templ", "views/**/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter Variable"', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        inner:
+          "inset 0 -0.25rem 0.25rem var(--tw-shadow-color, rgb(0 0 0 / .1))",
+      },
+    },
+    borderRadius: {
+      none: "0",
+      sm: "0.25rem",
+      DEFAULT: "0.25rem",
+    },
   },
   plugins: [],
 };
+// 'Inter Variable'
